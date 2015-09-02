31295961e78c2e66e47340969d1d2448725ef1e8
Jordan Moldow <jmoldow@box.com>
2015-09-01 22:58:49 -0700

    Use PY3 print function
    Print functions are far superior to print statements.

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,6 +1,8 @@
+from __future__ import print_function
+
 import json
 
 if __name__ == '__main__':
-  print 'Hello world'
-  print 'Hello Jordan and Melissa'
-  print 2*2
+  print('Hello world')
+  print('Hello Jordan and Melissa')
+  print(2*2)
